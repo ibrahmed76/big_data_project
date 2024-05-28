8308603a41056b1a82d577e0536376b2277ee0d2
OmarSamirz <omaredu1250@gmail.com>
2024-05-29 00:17:22 +0300

    Add Game Data

---
+++ classes/game_data.py
@@ -0,0 +1,47 @@
+from cassandra_db import CassandraSingleton
+
+class GameData:
+    def __init__(self, game_id, name, game_type, current_state, world_layout_details):
+        self.game_id = game_id
+        self.name = name
+        self.game_type = game_type
+        self.current_state = current_state
+        self.world_layout_details = world_layout_details
+        self.cassandra = CassandraSingleton()
+
+    @staticmethod
+    def insert_game(self):
+        cassandra = CassandraSingleton()
+        query = f"""
+        INSERT INTO game_data (game_id, name, type, current_state, world_layout_details)
+        VALUES ({self.game_id}, '{self.name}', '{self.game_type}', '{self.current_state}', {self.word_layout_details})
+        """
+        cassandra.execute_query(query)
+
+
+    @staticmethod
+    def fetch_game(query):
+        cassandra = CassandraSingleton()
+        cassandra.execute_query(query)
+
+    # Delete a game from the database
+    @staticmethod
+    def delete_game(game_type, game_id):
+        cassandra = CassandraSingleton()
+        query = f"""
+        DELETE FROM game_data WHERE type='{game_type}' AND game_id={game_id}
+        """
+        cassandra.execute_query(query)
+
+    def display_game_info(self):
+        print(f"Game ID: {self.game_id}")
+        print(f"Name: {self.name}")
+        print(f"Type: {self.game_type}")
+        print(f"Current State: {self.current_state}")
+        print("World Layout Details:")
+        for key, value in self.world_layout_details.items():
+            print(f"  {key}: {value}")
+
+
+
+
--- classes/main.py
+++ classes/main.py
@@ -1,5 +1,6 @@
 from game_analytics import GameAnalytics
 from player_statistics import PlayerStatistics
+from game_data import GameData
 
 def main():
     print(PlayerStatistics.get_all_player_stats(1))
